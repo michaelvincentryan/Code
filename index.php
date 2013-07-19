3f186e8bcfbf9347d0ddff8bdc4709c6246be050
Michael Ryan <michaelvincentryan@gmail.com>
2013-07-19 16:41:32 -0700

    changing scaffolding

--- index.php
+++ index.php
@@ -26,8 +26,9 @@
 	<body>
 		<h1>My Dashboard</h1>
 		
+		<div class="span6">
 		<ul class="nav nav-tabs">
-		  <li><a href="#hn" data-toggle="tab">hn</a> </li>
+		  <li class="active"><a href="#hn" data-toggle="tab">hn</a> </li>
 		  <li><a href="#reddit" data-toggle="tab">reddit</a></li>
 		  <li><a href="#test" data-toggle="tab">Test</a></li>
 		</ul>
@@ -114,102 +115,103 @@
 			</div>
 			
 		</div>
+	</div>
 		
 		<script type="text/javascript">
 		    jQuery(document).ready(function ($) {
 			$('#tabs').tab();
 		    });
 		</script>   
-		<table border='1' color='black'>		
-			<tr>
-				<th><h2>hacker news</h2></th>
-				<th><h2>reddit</h2></th>
-			</tr>
-			<tr>
-				<td>
-					<ul>
-						<?php
+#		<table border='1' color='black'>		
+#			<tr>
+#				<th><h2>hacker news</h2></th>
+#				<th><h2>reddit</h2></th>
+#			</tr>
+#			<tr>
+#				<td>
+#					<ul>
+#						<?php
 
-							$url = "https://news.ycombinator.com/news" ;
-							//start doing curl stuff
+#							$url = "https://news.ycombinator.com/news" ;
+#							//start doing curl stuff
 
-							$ch = curl_init();
+#							$ch = curl_init();
 
-							curl_setopt($ch, CURLOPT_URL, $url);
-							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-							curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-
-
-							//actually do the cURL
-							$html = curl_exec($ch);
-
-							curl_close($ch);
-
-							$dom = new DOMDocument();
-							$dom -> loadHTML($html);
-							$xpath = new DOMXPath($dom);
-							$links = $dom -> getElementsByTagName("td");
-							$max = $links->length;
-							for ($cnt=0; $cnt < $max; $cnt++){
-								$nodeVal = $links->item($cnt)->nodeValue;
-								if ($links->item($cnt)->getAttribute('class') == 'title'){
-									if ($links->item($cnt)->getAttribute('valign') != 'top'){
-										$test = $xpath->query('*',$links->item($cnt))->item(0);
-										$link = $test->getAttribute('href');							
-										echo '<li>' . '<a href=' . $link . ' target=_blank	>' . $nodeVal  . '</a>' . '</li>';
-									}						
-								}
-								else { continue; }
-							}
-
-						    ?>		
-					</ul>
-				</td>
-				<td valign="top">
-					<ul>
-						<?php
-
-							$url = "www.reddit.com" ;
-							//start doing curl stuff
-
-							$ch = curl_init();
-
-							curl_setopt($ch, CURLOPT_URL, $url);
-							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-							curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+#							curl_setopt($ch, CURLOPT_URL, $url);
+#							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+#							curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+#							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
 
-							//actually do the cURL
-							$html = curl_exec($ch);
+#							//actually do the cURL
+#							$html = curl_exec($ch);
 
-							curl_close($ch);
+#							curl_close($ch);
 
-							$dom = new DOMDocument();
-							$dom -> loadHTML($html);
-							$xpath = new DOMXPath($dom);
-							$divs = $dom->getElementsByTagName('div');
-							$max = $divs->length;
-							for ($cnt=0; $cnt < $max; $cnt++){
-								if ($divs->item($cnt)->getAttribute('id') == 'siteTable'){
-									$links = $divs->item($cnt)->getElementsByTagName('a');
-									$lmax = $links->length;
-									
-									for ($cnt=0; $cnt < $max; $cnt++){
-										if ($links->item($cnt)->getAttribute('class') == 'title '){
-											$nodeVal = $links->item($cnt)->nodeValue;
-											$href = $links->item($cnt)->getAttribute('href');
-											echo '<li> <a href=' . $href . ' target=_blank	>' . $nodeVal . '</a></li>';
-										}
-									}
-								}
-							}
-						    ?>		
-					</ul>
-				</td>
-			</tr>
-		</table>	
+#							$dom = new DOMDocument();
+#							$dom -> loadHTML($html);
+#							$xpath = new DOMXPath($dom);
+#							$links = $dom -> getElementsByTagName("td");
+#							$max = $links->length;
+#							for ($cnt=0; $cnt < $max; $cnt++){
+#								$nodeVal = $links->item($cnt)->nodeValue;
+#								if ($links->item($cnt)->getAttribute('class') == 'title'){
+#									if ($links->item($cnt)->getAttribute('valign') != 'top'){
+#										$test = $xpath->query('*',$links->item($cnt))->item(0);
+#										$link = $test->getAttribute('href');							
+#										echo '<li>' . '<a href=' . $link . ' target=_blank	>' . $nodeVal  . '</a>' . '</li>';
+#									}						
+#								}
+#								else { continue; }
+#							}
+
+#						    ?>		
+#					</ul>
+#				</td>
+#				<td valign="top">
+#					<ul>
+#						<?php
+
+#							$url = "www.reddit.com" ;
+#							//start doing curl stuff
+
+#							$ch = curl_init();
+
+#							curl_setopt($ch, CURLOPT_URL, $url);
+#							curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+#							curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+#							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+
+#							//actually do the cURL
+#							$html = curl_exec($ch);
+
+#							curl_close($ch);
+
+#							$dom = new DOMDocument();
+#							$dom -> loadHTML($html);
+#							$xpath = new DOMXPath($dom);
+#							$divs = $dom->getElementsByTagName('div');
+#							$max = $divs->length;
+#							for ($cnt=0; $cnt < $max; $cnt++){
+#								if ($divs->item($cnt)->getAttribute('id') == 'siteTable'){
+#									$links = $divs->item($cnt)->getElementsByTagName('a');
+#									$lmax = $links->length;
+#									
+#									for ($cnt=0; $cnt < $max; $cnt++){
+#										if ($links->item($cnt)->getAttribute('class') == 'title '){
+#											$nodeVal = $links->item($cnt)->nodeValue;
+#											$href = $links->item($cnt)->getAttribute('href');
+#											echo '<li> <a href=' . $href . ' target=_blank	>' . $nodeVal . '</a></li>';
+#										}
+#									}
+#								}
+#							}
+#						    ?>		
+#					</ul>
+#				</td>
+#			</tr>
+#		</table>	
 	</body>
 
 </html>
